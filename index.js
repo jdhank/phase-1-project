449a11133cb2ef9f3f6ff2378fd53c55ede6291a
Jake Hankins <jakehankins@comcast.net>
2022-06-06 20:11:53 -0400

    added random button

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 let btn= document.getElementById('btn')
+let output = document.getElementById('output')
+
+function displayRandomMovie(data){
+    let randomMovie = data[Math.floor(Math.random() * data.length)]
+    output.innerHTML = randomMovie.title
+
+}
 
 function getMovies(){
     fetch("http://localhost:3000/movies")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    
+    .then (response => response.json())
+    .then(data => {
+        console.log(data);
+        displayRandomMovie(data)
+    })
 }
-getMovies()
+
+
+
+
+
+
 btn.addEventListener('click', (event) => {
     event.preventDefault()
-   
+    console.log("clicked")
+    getMovies()
     
+    
+   
 })
 
+    
+   
+    
 
-function randomMovie(movies){
-    output.textContent = movies.title
-}
+
+
