551594b0c8f8656e4ee0c7efc1b6fa832f302227
Jake Hankins <jakehankins@comcast.net>
2022-06-08 19:57:01 -0400

    dropdown is working and click events added

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ let btn= document.getElementById('btn')
 let randomMovieTitle = document.getElementById('randomMovieTitle')
 let genreButton = document.getElementById('genreBtn')
 let randomMovieRating = document.getElementById('randomMovieRating')
+let comedy = document.getElementById('comedy')
+let drama = document.getElementById('drama')
+let family = document.getElementById('family')
+let horror = document.getElementById('horror')
+let action = document.getElementById('action')
 
 function displayRandomMovie(data){
     let randomMovie = data[Math.floor(Math.random() * data.length)]
     randomMovieTitle.innerHTML = randomMovie.title
     randomMovieRating.innerHTML = randomMovie.rating
-
-
-
-
 }
 
-function getMovies(){
+function getRandomMovies(){
     fetch("http://localhost:3000/movies")
     .then (response => response.json())
     .then(data => {
@@ -21,7 +22,13 @@ function getMovies(){
         displayRandomMovie(data)
     })
 }
-
+function getMoviesByGenre(){
+    fetch("http://localhost:3000/movies")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+}
 
 
 
@@ -29,13 +36,31 @@ function getMovies(){
 
 btn.addEventListener('click', (event) => {
     event.preventDefault()
-    console.log("clicked")
-    getMovies()
+    console.log("clicked random button")
+    getRandomMovies()
 })
 
 genreButton.addEventListener('click', (event) => {
-   
-    console.log("clicked")
-    M.AutoInit();
+    console.log("clicked genre button")
+    
+ 
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var options = {}
+
+
+    var instances = M.Dropdown.init(elems, options);
+  });
+
+  comedy.addEventListener('click', (event) => {
+    console.log("clicked comedy button")
+    getMoviesByGenre()
+
+
+})
+
+
+
 
