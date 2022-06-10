9387d691cdecad2b82fcfac4857503ffe4f7b7e2
Jake Hankins <jakehankins@comcast.net>
2022-06-10 16:36:08 -0400

    rendered data from dropdown menu

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ let drama = document.getElementById('drama')
 let family = document.getElementById('family')
 let horror = document.getElementById('horror')
 let action = document.getElementById('action')
+const genreList = document.getElementById("genre-list")
 
 function displayRandomMovie(data){
     let randomMovie = data[Math.floor(Math.random() * data.length)]
     randomMovieTitle.innerHTML = randomMovie.title
     randomMovieRating.innerHTML = randomMovie.rating
+}
+function displayMoviesByGenre(data){
+    const genres = data
+    .map(movie =>{
+        const li = document.createElement("li")
+        
+        li.textContent = movie.title 
+        genreList.append(li)
+         })
 }
 
 function getRandomMovies(){
@@ -26,14 +36,12 @@ function getMoviesByGenre(){
     fetch("http://localhost:3000/movies")
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-    })
-}
-
-
-
-
-
+       console.log(data)
+       displayMoviesByGenre(data)
+      })
+    
+    }
+    
 btn.addEventListener('click', (event) => {
     event.preventDefault()
     console.log("clicked random button")
@@ -49,17 +57,20 @@ genreButton.addEventListener('click', (event) => {
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var options = {}
-
-
     var instances = M.Dropdown.init(elems, options);
   });
 
   comedy.addEventListener('click', (event) => {
     console.log("clicked comedy button")
+    genreList.innerHTML = ""
     getMoviesByGenre()
+    const filteredMovie = genreList.filter(movie => 
+        movie.genre === "Comedy")
+    filteredMovie()
 
+    
 
-})
+  })
 
 
 
